5fe13ffa682765e3d7cfc0d184f1756016149fec
Damyan <damo.ts.04@gmail.com>
2019-12-15 03:14:04 +0200

    adding support for mobile and js events

---
+++ docs/scripts.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    var form=document.getElementById("form");
+  form.reset();
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  
+}
+function navfunc(){
+    var content=document.getElementById("menu").style.display;
+    console.log(content);
+    if(content=="flex"){
+        content=document.getElementById("menu").style.display = "none";
+        document.getElementById("title").style.display="flex";
+    }else{
+        content=document.getElementById("menu").style.display = "flex";
+        document.getElementById("title").style.display="none";
+    }
+
+   
+}
